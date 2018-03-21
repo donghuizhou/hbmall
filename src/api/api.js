10d7fddbc4d11988adaec06d8ceba1bed199b686
donghuizhou <765966797@qq.com>
2018-03-21 18:16:53 +0800

    sfd

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 let env = process.env.NODE_ENV || 'development'
 
 export const baseUrl = env === 'development' ? 'http://192.168.31.121:3000'
-  : 'http://hb.zhoudonghui.cn'
+  : 'http://api.zhoudonghui.cn'
 
 export let getProd = params => axios.get(baseUrl + '/hb/getProd', { params })
